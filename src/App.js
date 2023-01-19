5a019bea68ef8e20e213f919b4598f59f601624e
AbdulRehman <likeam99@gmail.com>
2023-01-19 18:44:49 +0500

    Action creater

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import SongList from "./components/SongList";
 
 function App() {
   return (
-    <div>
-      <h1>WelCome to App</h1>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import{ createStore } from 'redux';
 import App from './App';
+import reducers from './components/reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={createStore(reducers)}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
