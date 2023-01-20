d8b7aa94343b964aed9858f86082508308989684
AbdulRehman <likeam99@gmail.com>
2023-01-20 11:01:21 +0500

    Building select song

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import SongDetail from "./components/SongDetail";
 import SongList from "./components/SongList";
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import{ createStore } from 'redux';
 import App from './App';
-import reducers from './components/reducers';
+import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
